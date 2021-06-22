373f59068189a9eb6225f326334071bfd018127b
priyankalima <53181160+priyankalima@users.noreply.github.com>
2021-06-22 18:45:57 +0530

    Update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -21,7 +21,7 @@ export default function MyApp({ Component, pageProps }) {
   	<>
       <Head>
       <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-      <title>Demo</title>
+      <title>NewsPRK</title>
       <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/css/bootstrap.min.css" rel="stylesheet" />
       <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.1/dist/js/bootstrap.bundle.min.js" ></script>
       <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"/>
