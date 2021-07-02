053a38a44d6188301de9d57b6f7dcaa4710b4d71
priyankalima <53181160+priyankalima@users.noreply.github.com>
2021-07-02 20:05:37 +0530

    Update index.js

--- pages/index.js
+++ pages/index.js
@@ -1,20 +1,11 @@
+import Home from './components/Home';
 
-import dynamic from 'next/dynamic';
-const DymaicPage = dynamic(() => 
-  import('./components/Home')
-  .then((mod) => mod.Home),
-  {
-    loading: () =>
-    <div className="lazy-loader">Please wait...</div>,
-    ssr: false
-    }
-)
 
 const index = () => {
 
   return (
     <>
-      <DymaicPage />
+      <Home />
       
     </>
   )
